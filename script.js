9634ef57be63fedccbeaa6fd6115171c5878f1a1
lauree2 <lauree.mutua@gmail.com>
2023-06-27 17:21:14 +0300

    updated html,js

--- script.js
+++ script.js
@@ -1,25 +1,91 @@
-const form = document.getElementById('form')
+const firebaseConfig = {
+    apiKey: "AIzaSyCyX5s_SRUN6X-Fb7els1csIC3mHo-d8Bs",
+    authDomain: "final-project-test-d4c16.firebaseapp.com",
+    databaseURL: "https://final-project-test-d4c16-default-rtdb.firebaseio.com",
+    projectId: "final-project-test-d4c16",
+    storageBucket: "final-project-test-d4c16.appspot.com",
+    messagingSenderId: "304468731184",
+    appId: "1:304468731184:web:52c6517078e66e09e994f5",
+    measurementId: "G-CHDQFPEVSM"
+  };
 
-form.addEventListener('submit' ,function(event) {  
+  firebase.initializeApp(firebaseConfig);
+
+  var totalItem;
+  var maxCode;
+  var code;
+ 
+  
+  function storedata(event) {
     event.preventDefault();
-    const yourName = document.getElementById('Full_Name').value
-    const yourIDPassport = document.getElementById('ID/Passport_Number').value
-    const yourLocation=document.getElementById('County').value
-    const yourEmail=document.getElementById('Email_adress').value
+  
+    var fullName= document.getElementById("fullName").value;
+    var IDPassportNumber = document.getElementById("IDPassportNumber").value;
+    var Location= document.getElementById("Location").value;
+    var email= document.getElementById("email").value;
+    var IMEI= document.getElementById("IMEI").value;
+    var DeviceModel= document.getElementById("DeviceModel").value;
+    var DeviceBrand= document.getElementById("DeviceBrand").value;
 
-    console.log(yourName)
-    console.log(yourID/Passport)
-    console.log(location)
-    console.log(Email) 
-    })
-    form.addEventListener('submit' ,function(event) {  
-        event.preventDefault();
-    const Devicemodel = document.getElementById('Device_Model').value
-    const brand = document.getElementById('Device_Brand').value
-    const IMEI=document.getElementById('IMEI_Number').value
+    document.getElementById("fullName").value = "";
+    document.getElementById("IDPassportNumber").value = "";
+    document.getElementById("Location").value = "";
+    document.getElementById("Email").value = "";
+    document.getElementById("IMEI").value = "";
+    document.getElementById("DeviceModel").value = "";
+    document.getElementById("DeviceBrand").value = "";
 
-    console.log(model)
-    console.log(brand)
-    console.log(IMEI) 
-    })
+    //  store data to firebase
+  firebase
+  .database()
+  .ref("Registrationdetails/".code)
+  .set({
+    fullName: fullName,
+    IDPassportNumber: IDPassportNumber,
+    Location: Location,
+    Email: Email,
+    IMEI:IMEI,
+    DeviceModel:DeviceModel,
+    DeviceBrand: DeviceBrand,
+    status: "pending",
+  });
+
+
+  document.getElementById("registration form-header").insertAdjacentHTML(
+    "afterend",
+
+    `
+        <div class="registration details form" id="${code}">
+          <div class="data" id="${fullName}" >
+          <div class="data" id="${IDPassportNumber}" >
+          <div class="data" id="${Location}" >
+          <div class="data" id="${Email}}" >
+          <div class="data" id="${IMEI}" >
+          <div class="data" id="${DeviceModel}" >
+          <div class="data" id="${DeviceBrand}" >
+            <button class="done" id="done" onclick="changeStatus('${code}')" > <i class="far far-check-circle"></i> </button>
+            <h2 class="fullName">${fullName}</h2>
+            <p class="IDPassportNumber>${IDPassportNumber}</p>
+            <p class="Location">${Location}</p>
+            <p class="Email">${Email}</p>
+            <p class="IMEI">${IMEI}</p>
+            <p class="DeviceModel">${DeviceModel}</p>
+            <p class="DeviceBrand">${DeviceBrand}</p>
+
+            <p id="status"></p>
+          </div>
+          <hr>
+          <div class="buttons">
+            <button class=" button edit" id="editData" onclick = "editData('${code}')" >EDIT TASK</button>
+            <button class=" button delete" id="deleteData" onclick="deleteData('${code}')"  >DELETE TASK</button>
+          </div>
+        </div>
+      `
+  );
+
+
+}
+
+
+
 
