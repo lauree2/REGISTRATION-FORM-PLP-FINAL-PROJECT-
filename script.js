8f17e3d48fbcd473518dcf29cd23383647907f0f
lauree2 <lauree.mutua@gmail.com>
2023-06-27 21:59:25 +0300

    4TH COMMIT

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ const firebaseConfig = {
   var code;
  
   
-  function storedata(event) {
-    event.preventDefault();
+  function registerdetails(event) {
+    event.preventDefault()
   
     var fullName= document.getElementById("fullName").value;
     var IDPassportNumber = document.getElementById("IDPassportNumber").value;
@@ -26,7 +26,6 @@ const firebaseConfig = {
     var IMEI= document.getElementById("IMEI").value;
     var DeviceModel= document.getElementById("DeviceModel").value;
     var DeviceBrand= document.getElementById("DeviceBrand").value;
-
     document.getElementById("fullName").value = "";
     document.getElementById("IDPassportNumber").value = "";
     document.getElementById("Location").value = "";
@@ -36,10 +35,7 @@ const firebaseConfig = {
     document.getElementById("DeviceBrand").value = "";
 
     //  store data to firebase
-  firebase
-  .database()
-  .ref("Registrationdetails/".code)
-  .set({
+  firebase.database().ref("Registrationform/"+ code).set({
     fullName: fullName,
     IDPassportNumber: IDPassportNumber,
     Location: Location,
@@ -51,41 +47,9 @@ const firebaseConfig = {
   });
 
 
-  document.getElementById("registration form-header").insertAdjacentHTML(
-    "afterend",
+  //document.getElementById("registration form-header").insertAdjacentHTML(
+    //"afterend",
+   
+ 
 
-    `
-        <div class="registration details form" id="${code}">
-          <div class="data" id="${fullName}" >
-          <div class="data" id="${IDPassportNumber}" >
-          <div class="data" id="${Location}" >
-          <div class="data" id="${Email}}" >
-          <div class="data" id="${IMEI}" >
-          <div class="data" id="${DeviceModel}" >
-          <div class="data" id="${DeviceBrand}" >
-            <button class="done" id="done" onclick="changeStatus('${code}')" > <i class="far far-check-circle"></i> </button>
-            <h2 class="fullName">${fullName}</h2>
-            <p class="IDPassportNumber>${IDPassportNumber}</p>
-            <p class="Location">${Location}</p>
-            <p class="Email">${Email}</p>
-            <p class="IMEI">${IMEI}</p>
-            <p class="DeviceModel">${DeviceModel}</p>
-            <p class="DeviceBrand">${DeviceBrand}</p>
-
-            <p id="status"></p>
-          </div>
-          <hr>
-          <div class="buttons">
-            <button class=" button edit" id="editData" onclick = "editData('${code}')" >EDIT TASK</button>
-            <button class=" button delete" id="deleteData" onclick="deleteData('${code}')"  >DELETE TASK</button>
-          </div>
-        </div>
-      `
-  );
-
-
-}
-
-
-
-
+  }
